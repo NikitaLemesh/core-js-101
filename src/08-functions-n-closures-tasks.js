575ef27bf666cb6f365d3969243f7f730b91a700
Nikita Lemesh <nikita.lemesh.1991@mail.ru>
2022-07-25 18:03:59 +0300

    feat:add solution

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -84,8 +84,11 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const res = func(Math.random());
+  return function foo() {
+    return res;
+  };
 }
 
 
